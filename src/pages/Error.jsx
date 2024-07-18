d56361a5d3f789b76e0d6df0de39a88bea3bf5cc
winter <sohee@weconec.com>
2024-07-18 17:23:00 +0900

    404페이지 설정

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { css } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return <div css={wrapper}>Error</div>;
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    navigate("/");
+  };
+  return (
+    <div css={wrapper}>
+      <p>잘못된 경로입니다.</p>
+      <div css={btnWrapper} onClick={onClickHandler}>
+        홈으로 이동
+      </div>
+    </div>
+  );
 };
 
 export default Error;
@@ -13,4 +25,17 @@ const wrapper = css`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-family: "IM_Hyemin-Bold";
+  background-color: #f7e6e9;
+  color: #fb8971;
+  font-size: 24px;
+  gap: 20px;
+`;
+
+const btnWrapper = css`
+  padding: 19px 40px;
+  border-radius: 50px;
+  border: 2px solid #fb8971;
+  font-size: 18px;
+  cursor: pointer;
 `;
